bbb0ad061f543e1ee0a831df08b5325001841324
coco <cocorlawnghks@gmail.com>
2022-03-21 14:36:52 +0900

    FEAT: add Feed post api
    views만 함 urls아직안바금

--- backend/vuesta/views.py
+++ backend/vuesta/views.py
@@ -12,3 +12,10 @@ class FeedList(APIView):
         feed = Feed.objects.all()
         serializer = FeedSerializer(feed, many=True)
         return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = SnippetSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
